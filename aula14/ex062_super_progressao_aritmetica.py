0c6a6d67fdbf0fee9e6c240d0183e4d70b24db5c
André Azevedo <andreazevedo2014.aa@gmail.com>
2026-01-29 13:57:29 -0300

    Clean up comments in super arithmetic progression script
    Removed unnecessary comments and improved code clarity.

--- aula14/ex062_super_progressao_aritmetica.py
+++ aula14/ex062_super_progressao_aritmetica.py
@@ -1,10 +1,6 @@
 # Desafio 062: Super Progressão Aritmética v3.0
 # Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
 
-# É praticamente oque eu tinha pensado na opção 1 do ex061. 
-# 1º Coloquei tudo dentro do While pra fazer a repitação até a pessoa pedir pra parar
-# A minha 2º opção foi fazer sem a repetição, deixando as primeiras variaveis fora do while, pra não ter repetição.
-
 print('Gerador de PA')
 print('-=' * 10)
 primeiro = int(input('Primeiro termo: '))
